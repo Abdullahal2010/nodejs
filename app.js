34ef0fbd93c4c80dc72722e34d68e534a900339c
Abdullah Al Noman <111496179+Abdullahal2010@users.noreply.github.com>
2023-11-30 11:37:52 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
-
+const Comment = require("./models/user.model");
+require("./config/db");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  res.send("hello");
+  const allComments = await Comment.find();
+  res.send(allComments);
 });
 
 app.post("/", async (req, res) => {
-
+  const newComment = new Comment({
+    username: req.body.username,
+    comment: req.body.comment,
+  });
   await newComment.save();
-  res.send("new Comment");
+  res.send(newComment);
 });
 
 app.get("/hello", (req, res) => {
