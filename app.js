9e9486f6b8021fcb2711f3f821f644918fb5287a
Abdullah Al Noman <111496179+Abdullahal2010@users.noreply.github.com>
2023-11-30 10:42:11 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const Comment = require("./models/user.model");
-require("./config/db");
+
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
